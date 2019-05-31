977cfa9f5205a4b2dfc1783d92dd025bce781a61
Layne Faler <laynefaler@gmail.com>
2019-05-31 00:47:21 -0700

    Updates to my dev kit

--- mydevkit/Classes/MyDevKit.swift
+++ mydevkit/Classes/MyDevKit.swift
@@ -1,12 +1,9 @@
 import Alamofire
 
 class MyDevKit {
+    var hostname: String = "http://localhost:8081"
 
-    init() {
-        self.hostname: String = "http://localhost:8081"
-    }
-
-    register(public_key: String, application_key: String, device_token: String, custom_parameters: Parameters = []) {
+    func register(public_key: String, application_key: String, device_token: String, custom_parameters: Parameters = []) {
         let headers: HTTPHeaders = [
             "Authorization": public_key,
             "Accept": "application/json",
