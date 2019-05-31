c1f71bd03e57217d9a4e054e7577829720652821
Layne Faler <laynefaler@gmail.com>
2019-05-31 10:26:05 -0700

    Updates to my dev kit

--- mydevkit/Classes/MyDevKit.swift
+++ mydevkit/Classes/MyDevKit.swift
@@ -2,21 +2,16 @@ import Alamofire
 
 
 class MyDevKit {
-    var hostname: String = "https://api.mydevkit.io"
-    
-    public func register(public_key: String, application_key: String, device_token: String, custom_parameters: [String: Any] = [:]) {
+    public func register(public_key: String, application_key: String, device_token: String, custom_parameters: [String: Any] = [:], completionHandler: @escaping (DataResponse<Any>) -> Void) {
         let headers: HTTPHeaders = [
             "Authorization": public_key,
             "Accept": "application/json",
         ]
-    
+
         var parameters: Parameters = custom_parameters
         parameters["application_key"] = application_key
         parameters["device_token"] = device_token
 
-        Alamofire.request(self.hostname + "/api/v1/register/ios", method: .post, parameters: parameters, headers: headers).responseJSON { response in
-            debugPrint(response)
-        }
+        Alamofire.request("https://api.mydevkit.io/api/v1/register/ios", method: .post, parameters: parameters, headers: headers).responseJSON(completionHandler: completionHandler)
     }
-
 }
