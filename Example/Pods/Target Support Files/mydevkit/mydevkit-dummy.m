fae4a86a59a7828af560cfb78700bf6c59326d50
Layne Faler <laynefaler@gmail.com>
2019-05-31 00:30:09 -0700

    Initial commit

---
+++ Example/Pods/Target Support Files/mydevkit/mydevkit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_mydevkit : NSObject
+@end
+@implementation PodsDummy_mydevkit
+@end
---
+++ mydevkit/Classes/MyDevKit.swift
@@ -0,0 +1,24 @@
+import Alamofire
+
+class MyDevKit {
+
+    init() {
+        self.hostname: String = "http://localhost:8081"
+    }
+
+    register(public_key: String, application_key: String, device_token: String, custom_parameters: Parameters = []) {
+        let headers: HTTPHeaders = [
+            "Authorization": public_key,
+            "Accept": "application/json",
+        ]
+
+        var parameters: Parameters = custom_parameters
+        parameters["application_key"] = application_key
+        parameters["device_token"] = device_token
+
+        Alamofire.request(self.hostname + "/api/v1/register/ios", method: .post, parameters: parameters, headers: headers).responseJSON { response in
+            debugPrint(response)
+        }
+    }
+
+}
